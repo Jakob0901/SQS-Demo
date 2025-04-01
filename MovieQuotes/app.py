53aa498551a58a0852d7a8f821d372b983556644
Jakob Kellhuber <jakob.kellhuber@stud.th-rosenheim.de>
2025-04-01 19:22:26 +0200

    initial app implementation

---
+++ MovieQuotes/app.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask, request, jsonify, render_template
+from functools import wraps
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
+app.config['WTF_CSRF_ENABLED'] = True
+csrf = CSRFProtect(app)
+
+API_KEY = os.environ.get('API_KEY')
+
+def require_api_key(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if request.headers.get('x-api-key') == API_KEY:
+            return f(*args, **kwargs)
+        else:
+            return jsonify({"message": "Forbidden"}), 403
+
+    return decorated_function
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/quote')
+def get_quote():
+    return "You are a wizard, Harry!"
+
+@app.route('/stored_quotes')
+@require_api_key
+def get_stored_quotes():
+    return "Quotes will be stored here!"
+
+@app.route('/save', methods=['POST'])
+@require_api_key
+def save_quote():
+    data = request.form.get('quote')
+    print(data)
+    return "Quote saved!"
+
+if __name__ == '__main__':
+    if not API_KEY:
+        raise ValueError("No API_KEY environment variable set")
+
+    app.run(ssl_context='adhoc')
---
+++ MovieQuotes/models/FavoriteQuote.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Text, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+
+class FavoriteQuote(Base):
+    __tablename__ = 'favorite_quotes'
+
+    id = Column(String, primary_key=True)
+    quote = Column(Text, nullable=False)
+    movie = Column(String(255), nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
