ec3e7fe2927ab85c78463eb17888830af5d1515c
Jakob Kellhuber <jakob.kellhuber@stud.th-rosenheim.de>
2025-05-31 18:57:59 +0200

    add path fix to remaining tests

--- MovieQuotes/tests/test_favorite_quotes.py
+++ MovieQuotes/tests/test_favorite_quotes.py
@@ -1,5 +1,10 @@
+import os
+import sys
 import unittest
 from datetime import datetime
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from MovieQuotes.models.FavoriteQuote import FavoriteQuote
 
 class TestFavoriteQuote(unittest.TestCase):
